b1a20753eb1e9bf1990dfc4c3bc7587fc117c853
Jefwillems <jefwillems100@gmail.com>
2020-11-25 16:25:46 +0100

    update api with better support for updating, creating codes

--- api/serializers.py
+++ api/serializers.py
@@ -6,36 +6,45 @@ class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
-        read_only_fields = ['name']
+        extra_kwargs = {
+            'name': {'validators': []},
+        }
 
 
 class LinkUrlSerializer(serializers.ModelSerializer):
     class Meta:
         model = LinkUrl
-        exclude = ['code']
+        exclude = ['code', 'id']
 
 
 class QRCodeSerializer(serializers.ModelSerializer):
     urls = LinkUrlSerializer(many=True)
-
-    class Meta:
-        model = QRCode
-        fields = '__all__'
-        read_only_fields = ['created', 'last_updated', 'urls']
+    department = DepartmentSerializer(required=True)
 
     def create(self, validated_data):
-        link_urls = validated_data.pop('urls')
-        code = super(QRCodeSerializer, self).create(validated_data)
-        for url in link_urls:
+        urls_data = validated_data.pop('urls')
+        dep_data = validated_data.pop('department')
+        department, created = Department.objects.get_or_create(**dep_data)
+        code = QRCode.objects.create(**validated_data, department=department)
+        for url in urls_data:
             LinkUrl.objects.create(code=code, **url)
         return code
 
     def update(self, instance, validated_data):
-        link_urls = validated_data.pop('urls')
-        code = super(QRCodeSerializer, self).update(instance, validated_data)
-        for url in link_urls:
-            LinkUrl.objects.get_or_create(code=code, **url)
-        return code
+        urls_data = validated_data.pop('urls')
+        dep_data = validated_data.pop('department')
+        department, created = Department.objects.get_or_create(**dep_data)
+        instance.department = department
+        # TODO: This can be cleaner
+        LinkUrl.objects.filter(code=instance).delete()
+        for url in urls_data:
+            LinkUrl.objects.create(code=instance, **url)
+        return instance
+
+    class Meta:
+        model = QRCode
+        fields = '__all__'
+        read_only_fields = ['created', 'last_updated']
 
 
 class ApiHitSerializer(serializers.ModelSerializer):
