5b1b2374d87c32ba5054dec52fcea9488483140a
Jefwillems <jefwillems100@gmail.com>
2020-12-09 17:02:01 +0100

    disable browsable api in production

--- QRcodeAPI/settings/production.py
+++ QRcodeAPI/settings/production.py
@@ -3,3 +3,18 @@ import os
 
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+}
